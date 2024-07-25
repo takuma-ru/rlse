c23af11f6196aa695d8bab288dd3d715cecc53bb
takuma-ru <kanekotakuma@team-lab.com>
2024-07-25 12:50:35 +0900

    🐛 `git branch --show-current` cmd option

--- packages/core/src/action/releaseAction.ts
+++ packages/core/src/action/releaseAction.ts
@@ -7,7 +7,13 @@ import { packageVersionUp } from "./packageVersionUp";
 import { findPackageJsonByName } from "./findPackageJsonByName";
 
 export const releaseAction = async (options: unknown) => {
-  const baseBranch = cmd(`git branch --show-current`).trim();
+  const baseBranch = cmd(`git branch --show-current`, {
+    execOptions: {
+      stdio: "pipe",
+      encoding: "utf8",
+    },
+    successCallback: (stdout) => stdout.trim(),
+  });
   const releaseBranch = `release/${new Date()
     .toISOString()
     .replace(/[-:.]/g, "_")}`;

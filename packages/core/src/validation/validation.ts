b5ac6262b4ddfa5dab997a45a4fca08eac2ec0da
takuma-ru <kanekotakuma@team-lab.com>
2024-08-06 13:36:20 +0900

    🐛 email validation

--- packages/core/src/validation/validation.ts
+++ packages/core/src/validation/validation.ts
@@ -20,7 +20,12 @@ export const releaseSchema = z.object({
     message: "Invalid build command",
   }),
   gitUserName: z.string().optional(),
-  gitUserEmail: z.string().email().optional(),
+  gitUserEmail: z
+    .string()
+    .regex(
+      /^(?!\.)(?!.*\.\.)([A-Z0-9_+-\.]*)[A-Z0-9_+-]@([A-Z0-9][A-Z0-9\-]*\.)+[A-Z]{2,}$/i
+    )
+    .optional(),
   dryRun: z.boolean().optional().default(false),
   noRun: z.boolean().optional().default(false),
 });
